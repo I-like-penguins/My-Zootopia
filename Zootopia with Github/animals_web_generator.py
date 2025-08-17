a9570844b18e4a7432f2f1b1a68115040c4a7ed5
thomasrennefeld <ThomasRe@mailbox.org>
2025-08-17 11:12:00 +0200

    Access Data from JSON file

--- Zootopia with Github/animals_web_generator.py
+++ Zootopia with Github/animals_web_generator.py
@@ -1,1 +1,22 @@
-# Test
+import json
+
+def load_data(file_path):
+  """ Loads a JSON file """
+  with open(file_path, "r") as handle:
+    return json.load(handle)
+
+def main():
+    animals_data = load_data("animals_data.json")
+    for animal in animals_data:
+        print(f"Name: {animal['name']}")
+        print(f"Diet: {animal['characteristics']['diet']}")
+        print(f"Location: {animal['locations'][0]}")
+        try:
+            print(f"Type: {animal['characteristics']['type']}")
+        except KeyError:
+            print(f"Type: Not specified")
+        print("")
+
+
+if __name__ == "__main__":
+    main()
